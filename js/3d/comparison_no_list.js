5406069defc15086569cb5ba2bc58c71937f739c
robin <robin.bruneau@gmail.com>
2025-05-23 15:18:34 +0200

    update

--- js/3d/comparison_no_list.js
+++ js/3d/comparison_no_list.js
@@ -108,13 +108,13 @@ $(document).ready(() => {
 
 
     // Définir la position initiale de la caméra pour modelViewerComparison1
-    modelViewerComparison1.cameraOrbit = "324deg 92deg 1.21m"; // Exemple: vue de dessus, à 1.5m
-    modelViewerComparison1.cameraTarget = "0m 0m 0m";    // Exemple: regarder un peu plus haut
+    modelViewerComparison1.cameraOrbit = "-36deg 92deg 1.21m"; // Exemple: vue de dessus, à 1.5m
+    modelViewerComparison1.cameraTarget = "0.06m 0.05m 0.08m";    // Exemple: regarder un peu plus haut
     modelViewerComparison1.fieldOfView = "50deg";         // Exemple: champ de vision de 50 degrés
 
     // Vous pouvez faire de même pour modelViewerComparison2
-    modelViewerComparison2.cameraOrbit = "324deg 92deg 1.21m"; // Exemple: vue diagonale
-    modelViewerComparison2.cameraTarget = "0m 0m 0m";
+    modelViewerComparison2.cameraOrbit = "-36deg 92deg 1.21m"; // Exemple: vue diagonale
+    modelViewerComparison2.cameraTarget = "0.06m 0.05m 0.08m";
     modelViewerComparison2.fieldOfView = "50deg";
 
     modelViewerComparison1.isTextured = false;
